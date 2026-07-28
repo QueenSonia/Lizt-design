89b212e68493cc94cea91898fd2836a64eaba921
QueenSonia <soniaakpati7@gmail.com>
2026-07-28 15:23:28 +0100

    Increase spacing above footer on pay-installment demo

--- src/components/designs/PayInstallmentDemo.tsx
+++ src/components/designs/PayInstallmentDemo.tsx
@@ -358,7 +358,7 @@ export default function PayInstallmentDemo() {
           )}
 
           {/* Footer */}
-          <div className="h-[1px] bg-gray-200 mt-10 mb-4" />
+          <div className="h-[1px] bg-gray-200 mt-14 mb-4" />
           <div className="text-center text-[10px] leading-[14px] text-gray-400">
             <p>{landlordBranding.businessName}</p>
             <p>
